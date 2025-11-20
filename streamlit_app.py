0c7ca80aa54d3d899e3dc1aca74c1be8a54e4440
Ken Vellian <kvellian@Kens-MacBook-Pro.local>
2025-11-19 20:33:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,8 +171,8 @@ st.markdown(
     """
 **Created by:** Ken Vellian
   
-**Data Science Capstone Project:**
-**Harry Potter Retrieval-Augmented Generation (RAG) System**  
+**Data Science Capstone:**
+`Harry Potter Retrieval-Augmented Generation (RAG) System`  
 
 **Professor:** David Hubbard
 
